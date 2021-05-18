b64c27715e481c94c7540bdefa65df585ed67919
Matthew Stratford <hi@mstratford.net>
2021-05-18 22:05:56 +0100

    Update servers for first beta.

--- store.js
+++ store.js
@@ -19,21 +19,21 @@ const schema = {
     type: "object",
     default:
       {
-        "Localhost Hi": {
-          protocol: "http://",
-          host: "localhost",
-          port: 13500,
-          suffix: "/presenter/"
-        },
-        "MATTSicle": {
-          protocol: "http://",
-          host: "192.168.0.78",
-          port: 13500,
-          suffix: "/presenter/"
-        },
         "Studio Red": {
           protocol: "http://",
           host: "studio1.ury.york.ac.uk",
+          port: 13500,
+          suffix: "/presenter/"
+        },
+        "Studio Blue": {
+          protocol: "http://",
+          host: "studio2.ury.york.ac.uk",
+          port: 13500,
+          suffix: "/presenter/"
+        },
+        "Localhost (Dev)": {
+          protocol: "http://",
+          host: "localhost",
           port: 13500,
           suffix: "/presenter/"
         }
